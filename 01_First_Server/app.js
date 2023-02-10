73abfa226d1cdaf0484ccd1274c530494f59465e
PhilipNS98 <90253330+PhilipNS98@users.noreply.github.com>
2023-02-10 11:02:09 +0100

    Added  new file called app.js

---
+++ 01_First_Server/app.js
@@ -0,0 +1,56 @@
+//import and instansiate
+const app = require("express")();
+
+/*  const express = require("express");
+ const app = express(); */
+
+// route (entire thing)
+// HTTP method
+//  |   endpoint    callback function
+app.get("/", (req, res) => {
+    res.send({ message: "Our first route" });
+});
+
+let bicycleSpins = 0;
+app.get("/spinthebicycle", (req, res) => {
+    bicycleSpins += 1;
+    res.send({
+        message: `People have spun the bicycle wheel 
+    ${bicycleSpins} times`
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.send(`
+    <h1>About</h1>
+    <h3>This is my about page.</h3>
+    `);
+});
+
+
+
+
+
+
+
+/**assignment
+ *  create a new route that kicks the dinosaur
+ *  and the n the dinosaur says ow ow ow
+ */
+let kicks = 0
+let ows = 0;
+app.get("/kickthedinosaur", (req, res) => {
+    kicks += 1;
+    ows += 1;
+    res.send({
+        message: `Dinosaur has been kicked 
+        ${kicks} times`,
+        dinosaur: `dinosaur said ow ow ow ${ows} times`
+    }); 
+});
+
+
+
+
+//listen to the port 8080 (dev port)
+app.listen(8080);
