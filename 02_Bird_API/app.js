06a286504454352e7b03d9ae8f44364d6ffe936c
PhilipNS98 <90253330+PhilipNS98@users.noreply.github.com>
2023-02-16 20:30:47 +0100

    Added the missing get request for the bird api and wrote some documentation to how i understand it

--- 02_Bird_API/app.js
+++ 02_Bird_API/app.js
@@ -6,4 +6,20 @@ app.get("/birds", (req,res) =>{
     res.send({birds})
 })
 
+//defining the route for /birds/:id where :id is a dynamic route parameter that captures the bird id.
+app.get('/birds/:id', (req, res) => {
+    //using rew.params.id to get the id from the request url and wrapping it in Number() to make sure it is a number.
+    const birdId = Number(req.params.id);
+    //using Array.prototype.find() to look for the bird in the birds array whose id property matches the captured id
+    const bird = birds.find((b) => b.id === birdId);
+  
+    //if a bird is found, send bird back as a JSON response using res.send
+    if (bird) {
+      res.send(bird);
+      // if not found return a 404 status code and a message using res.status and res.send
+    } else {
+      res.status(404).send('Bird not found');
+    }
+  });
+
 app.listen(8080);
