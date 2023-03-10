80d785815bfa7997ad7268b01376d6b492f6ff0e
PhilipNS98 <90253330+PhilipNS98@users.noreply.github.com>
2023-03-10 09:22:30 +0100

    Server side redirection

--- 03_Plain_HTML_Server/app.js
+++ 03_Plain_HTML_Server/app.js
@@ -64,6 +64,14 @@ app.put("/api/visitors", (req, res) => {
     res.send({ data: ++visitorCount });
 })
 
+//Server side redirection
+app.get("/api/guards", (req, res) => {
+    if (req.query.passport === "theskyisblue") {
+       return res.redirect("/api/tanks");
+    } 
+        res.send({ message: "You are not allowed to see the tanks. Give us the secret in the query string  with the key being passport" })
+    
+});
 //POST
 
 //PUT
