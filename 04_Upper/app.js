6850dce927b43d2de931b6115724a9793d95efa9
PhilipNS98 <90253330+PhilipNS98@users.noreply.github.com>
2023-03-17 09:33:38 +0100

    added html concatination

--- 04_Upper/app.js
+++ 04_Upper/app.js
@@ -5,22 +5,49 @@ const app = express();
 app.use(express.static("public"));
 
 import jokes from "./util/jokes.js"
-console.log(await jokes.getJoke());
-
-//Pages (routes)
+/* console.log(await jokes.getJoke());
+ */
 
 const pagesRoot = "public/pages/"
 
+
+
+import fs from "fs";
+// Components
+// task read the navbar and the footer here
+const navbar = fs.readFileSync("./public/components/navbar/navbar.html").toString();
+const footer = fs.readFileSync("./public/components/footer/footer.html").toString();
+
+//Pages (routes)
+const frontpage = fs.readFileSync(`${pagesRoot}frontpage/frontpage.html`).toString();
+// task read the other files and serve them
+const IRLQuests = fs.readFileSync(`${pagesRoot}IRLQuests/IRLQuests.html`).toString();
+const jokespage = fs.readFileSync(`${pagesRoot}jokes/jokes.html`).toString();
+
+
+
+
+// Constructed pages
+const frontpagePage = navbar.replace("%%DOCUMENTTITLE%%", "Upper | Motivation") + frontpage + footer;
+const IRLQuestsPage = navbar.replace("%%DOCUMENTTITLE%%", "Upper | IRLQuests") + IRLQuests + footer;
+const jokespagePage = navbar.replace("%%DOCUMENTTITLE%%", "Upper | Jokes") + jokespage + footer;
+
+
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve("public/pages/frontpage/frontpage.html"));
+   res.send(frontpagePage);
 });
 
 app.get('/IRLQuests', (req, res) => {
-res.sendFile(path.resolve(pagesRoot, "IRLQuests/IRLQuests.html"));
+/* res.sendFile(path.resolve(pagesRoot, "IRLQuests/IRLQuests.html"));
+ */
+res.send(IRLQuestsPage);
+
 });
 
 app.get('/jokes', (req, res) => {
-    res.sendFile(path.resolve(pagesRoot, 'jokes/jokes.html'))
+/*     res.sendFile(path.resolve(pagesRoot, 'jokes/jokes.html'))
+ */
+res.send(jokespagePage);
 });
 
 //API
