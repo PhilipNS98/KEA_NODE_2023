c9465a48cec65bc97b6e1fe4c0d3bed40b1692cd
PhilipNS98 <90253330+PhilipNS98@users.noreply.github.com>
2023-03-17 08:45:49 +0100

    added proxy server for google.com

--- 03_Plain_HTML_Server/app.js
+++ 03_Plain_HTML_Server/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+
+const https = require('https');
+
+
 // Tells the Express server to serve static files from the "public" directory.
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -30,6 +34,41 @@ app.get("/visitors", (req, res) => {
 app.get("/museumGuards", (req, res) => {
     res.sendFile(__dirname + "/public/museumGuards/museumGuards.html");
 })
+//proxy server
+// ANOTHER WAY TO PROXY SERVE GOOGLE.COM!
+
+/*
+This endpoint '/proxy' makes a GET request to the 'https://www.google.com' URL.
+It uses the https module to send the request and receive the response.
+The response is retrieved in chunks through the 'data' event and concatenated into a string.
+When the response ends, the resulting string is sent back as a response to the client using the res.send() method.
+If there's an error during the request, it is logged to the console and an error message is sent back to the client.
+*/
+/* app.get('/proxy', (req, res) => {
+    https.get('https://www.google.com', (response) => {
+      let data = '';
+  
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
+  
+      response.on('end', () => {
+        res.send(data);
+      });
+    }).on('error', (error) => {
+      console.error(error);
+      res.send('Error occurred while making request to Google');
+    });
+  }); */
+  app.get('/proxy', (req, res) => {
+    // task make a request to https://www.google.com
+    // task serve the text data
+    fetch('https://www.google.com')
+    .then(response => response.text())
+    .then(result => res.send(result));
+  });
+
+
 //API
 
 //GET COUNTDOWN
