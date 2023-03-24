d29f43f01deb463fc802583f911992543dabac29
PhilipNS98 <90253330+PhilipNS98@users.noreply.github.com>
2023-03-24 11:52:13 +0100

    Created a svelte app

---
+++ 05_First_Svelte_Project/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import FavoriteBirds from "./lib/FavoriteBirds.svelte";
+    import Garden from "./lib/Garden.svelte";
+    import GardenGate from "./lib/GardenGate.svelte";
+  import Greeter from "./lib/Greeter.svelte";
+</script>
+
+<Greeter />
+<Greeter name="Uncle Bob" greeting="Goodbye" />
+<Greeter name="Martin" />
+
+<FavoriteBirds />
+<Garden />
+<GardenGate />
---
+++ 05_First_Svelte_Project/src/lib/FavoriteBirds.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Bird from "./Bird.svelte";
+
+    const birds = ["Dove", "Owl", "Turkey", "Eagle", "Dodo"];
+    const birdEmojis = ["🕊️", "🦉", "🦃", "🦅", "🦤"];
+
+    let birdIndex = 0;
+    function changeFavoriteBird() {
+        birdIndex = (birdIndex + 1) % birds.length;
+    }
+</script>
+
+<h2>Favorite Bird</h2>
+
+<div on:click={changeFavoriteBird} on:keydown={changeFavoriteBird}>
+    <Bird
+    name={birds[birdIndex]} 
+    emoji={birdEmojis[birdIndex]} 
+    />
+</div>
+
+<h2>All birds that i know of </h2>
+{#each birds as bird}
+<Bird name={bird} emoji={"???"} />
+{/each}
