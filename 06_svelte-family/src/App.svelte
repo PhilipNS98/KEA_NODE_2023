64364caf5d5bbda338d2c7a94d7457813658ba8e
PhilipNS98 <90253330+PhilipNS98@users.noreply.github.com>
2023-03-31 12:27:27 +0200

    Serve-side rednering of the svelte app

--- 06_svelte-family/src/App.svelte
+++ 06_svelte-family/src/App.svelte
@@ -1,6 +1,24 @@
 <script>
+  import { onMount } from "svelte";
+  import { BASE_URL } from "./store/urlDomain";
+
   import Home from "./components/Home/Home.svelte";
-    import Pibling from "./components/Pibling/Pibling.svelte";
+  import Pibling from "./components/Pibling/Pibling.svelte";
+
+
+  onMount(async () => {
+      const registerURL = `${$BASE_URL}/gotham/batman?hobby=catching the bad guys.`;
+      const registerResponse = await fetch(registerURL, { 
+        credentials: "include"
+      });
+      const batmanRegisteredMessage = await registerResponse.json();
+      
+      const registeredResponse = await fetch(`${$BASE_URL}/gotham`, { 
+        credentials: "include"
+      });
+      const registeredResponseMessage = await registeredResponse.json();
+      console.log(registeredResponseMessage);
+    });
 
   const piblings = ["John", "Mark"];
   const niblings = [
@@ -9,7 +27,7 @@
       name: "Ann",
       isGirl: true,
       allowance: 100,
-      cookies: []
+      cookies: [],
     },
     {
       id: 2,
@@ -17,23 +35,20 @@
       isGirl: true,
       blackSheep: "medium-black-sheep",
       allowance: 50,
-      cookies: []
+      cookies: [],
     },
     {
       id: 3,
       name: "Peter",
       blackSheep: "ultra-black-sheep",
       allowance: 75,
-      cookies: []
-    }
+      cookies: [],
+    },
   ];
-
-
 </script>
-
 
 <Home>
   <h2>This is the home of the Svelte Family</h2>
-  <Pibling name={piblings[0]} niblings={niblings} />
-  <Pibling name={piblings[1]} niblings={niblings} />
+  <Pibling name={piblings[0]} {niblings} />
+  <Pibling name={piblings[1]} {niblings} />
 </Home>
