1acea301a79dfca2bee0a072d975d7f0938c0d4b
PhilipNS98 <90253330+PhilipNS98@users.noreply.github.com>
2023-04-04 17:02:25 +0200

    fixed a few minor mistakes

--- 10_Mandatory1/app.js
+++ 10_Mandatory1/app.js
@@ -14,38 +14,50 @@ app.use(session({
 }));
 
 const homePage = templateEngine.readPage(`home/home.html`);
-const homepagePage = templateEngine.renderPage(homePage, { tabTitle: "Mandatory | Home",
-cssLink: '<link rel="stylesheet" href="/assets/css/home.css">' });
+const homepagePage = templateEngine.renderPage(homePage, {
+    tabTitle: "Mandatory | Home",
+    cssLink: '<link rel="stylesheet" href="/assets/css/home.css">'
+});
 
 const expresspage = templateEngine.readPage(`express/express.html`);
-const expresspagePage = templateEngine.renderPage(expresspage, { tabTitle: "Mandatory | Express",
-cssLink: '<link rel="stylesheet" href="/assets/css/express.css">' });
+const expresspagePage = templateEngine.renderPage(expresspage, {
+    tabTitle: "Mandatory | Express",
+    cssLink: '<link rel="stylesheet" href="/assets/css/express.css">'
+});
 
 const restapi = templateEngine.readPage(`restapi/restapi.html`);
-const restapiPage = templateEngine.renderPage(restapi, { tabTitle: "Mandatory | Rest API",
-cssLink: '<link rel="stylesheet" href="/assets/css/express.css">' });
+const restapiPage = templateEngine.renderPage(restapi, {
+    tabTitle: "Mandatory | Rest API",
+    cssLink: '<link rel="stylesheet" href="/assets/css/express.css">'
+});
 
 const javascript = templateEngine.readPage(`javascript/javascript.html`);
-const javascriptPage = templateEngine.renderPage(javascript, { tabTitle: "Mandatory | JavaScript",
-cssLink: '<link rel="stylesheet" href="/assets/css/express.css">' });
+const javascriptPage = templateEngine.renderPage(javascript, {
+    tabTitle: "Mandatory | JavaScript",
+    cssLink: '<link rel="stylesheet" href="/assets/css/express.css">'
+});
 
 const terminal = templateEngine.readPage(`terminal/terminal.html`);
-const terminalPage = templateEngine.renderPage(terminal, { tabTitle: "Mandatory | Terminal Commands",
-cssLink: '<link rel="stylesheet" href="/assets/css/express.css">' });
+const terminalPage = templateEngine.renderPage(terminal, {
+    tabTitle: "Mandatory | Terminal Commands",
+    cssLink: '<link rel="stylesheet" href="/assets/css/express.css">'
+});
 
 const IRLQuests = templateEngine.readPage(`IRLQuests/IRLQuests.html`);
 const IRLQuestsPage = templateEngine.renderPage(IRLQuests, { tabTitle: "Mandatory | IRLQuests" });
 
 const contact = templateEngine.readPage("contact/contact.html");
-const contactPage = templateEngine.renderPage(contact, {tabTitle: "Mandatory | Contact"});
+const contactPage = templateEngine.renderPage(contact, { tabTitle: "Mandatory | Contact" });
 
 const login = templateEngine.readPage("login/login.html");
-const loginPage = templateEngine.renderPage(login, {tabTitle: "Mandatory | Login", 
-cssLink: '<link rel="stylesheet" href="/assets/css/login.css">' });
- 
+const loginPage = templateEngine.renderPage(login, {
+    tabTitle: "Mandatory | Login",
+    cssLink: '<link rel="stylesheet" href="/assets/css/login.css">'
+});
+
 const adminPanel = templateEngine.readPage("adminPanel/adminPanel.html");
-const adminPanelPage = templateEngine.renderPage(adminPanel, {tabTitle: "Mandatory | Admin Panel"});
- 
+const adminPanelPage = templateEngine.renderPage(adminPanel, { tabTitle: "Mandatory | Admin Panel" });
+
 // Constructed pages
 
 app.get('/', (req, res) => {
@@ -76,18 +88,18 @@ app.get('/IRLQuests', (req, res) => {
 app.get('/jokes', async (req, res) => {
     const joke = await getJoke();
     const jokes = templateEngine.readPage(`jokes/jokes.html`)
-    .replace("$JOKE", JSON.stringify(joke));
+        .replace("$JOKE", JSON.stringify(joke));
     const jokespagePage = templateEngine.renderPage(jokes,
         {
             tabTitle: "Upper | Jokes",
             cssLink: '<link rel="stylesheet" href="/pages/jokes/jokes.css">'
         });
 
-        res.send(jokespagePage);
-    });
-    
-    app.get("/contact", (req, res) => {
-res.send(contactPage);
+    res.send(jokespagePage);
+});
+
+app.get("/contact", (req, res) => {
+    res.send(contactPage);
 })
 
 
@@ -98,12 +110,12 @@ app.get('/login', (req, res) => {
 app.get('/logout', (req, res) => {
     req.session.destroy();
     res.redirect('/');
-  });
-  
-  app.get('/adminPanel', (req, res) => {
+});
+
+app.get('/adminPanel', (req, res) => {
     console.log(req.session)
     res.send(adminPanelPage);
-  });
+});
 
 
 
@@ -111,13 +123,13 @@ app.get('/logout', (req, res) => {
 
 app.post("/api/contact", (req, res) => {
     console.log(req.body);
-//  res.send(req.body);
+    //  res.send(req.body);
     res.redirect("/");
 })
 
 import users from "./util/login.js";
 
-  app.post('/login', (req, res) => {
+app.post('/login', (req, res) => {
     const { username, password } = req.body;
     const user = users.find((user) => username === user.username);
 
@@ -129,10 +141,10 @@ import users from "./util/login.js";
         res.send("Failed login");
     }
 });
-  
+
 app.get('/api/isLoggedIn', (req, res) => {
     const username = req.session.user;
-    res.send({isLoggedIn: Boolean(username)});
+    res.send({ isLoggedIn: Boolean(username) });
 })
 
 
