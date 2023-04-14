3fbb64ffe66ef88d3839a410f0b76530a6b1e656
PhilipNS98 <90253330+PhilipNS98@users.noreply.github.com>
2023-04-14 10:47:17 +0200

    rate limitter

--- 07_Sessions/app.js
+++ 07_Sessions/app.js
@@ -30,13 +30,26 @@ app.use(session({
 
   import rateLimit from 'express-rate-limit'
 
+  //general rate limitter that applies to all routes
   const apiLimiter = rateLimit({
       windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+      max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
       standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
       legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   });
   app.use(apiLimiter);
+
+  // specific rate limitter that applies to a specific route (more restrictive)
+  app.use("/auth", rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 4, 
+    standardHeaders: true, 
+    legacyHeaders: false, 
+}));
+
+import authRouter from "./routers/authRouter.js"
+app.use(authRouter);
+
 
 import gothamRouter from "./routers/gothamRouter.js";
 app.use(gothamRouter);
