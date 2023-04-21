fb4f1466c42c34a7d0687a803077ac2a51361b6a
PhilipNS98 <90253330+PhilipNS98@users.noreply.github.com>
2023-04-21 09:17:42 +0200

    new commit

---
+++ 09_Solar_System/client/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { Router, Link, Route } from "svelte-navigator";
+  import Home from "./pages/Home/Home.svelte";
+    import Planets from "./pages/Planets/Planets.svelte";
+</script>
+
+<Router>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/planets">Planets</Link>
+  </nav>
+  <div>
+    <Route path="/">
+      <Home />
+    </Route>
+    <Route path="/planets">
+      <Planets/>
+    </Route>
+  </div>
+</Router>
---
+++ 09_Solar_System/server/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+const app = express();
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () => console.log("Server is running on port", server.address().port));
+
+
+
+
+
+
+
+
